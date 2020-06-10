591cde7fa94f1d74c8b43247efd88f09b0e89407
Paul Solt <PaulSolt@gmail.com>
2020-06-10 17:57:59 -0400

    Added starter project

---
+++ MapsAppPrototype/MapsAppPrototype/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MapsAppPrototype
+//
+//  Created by Paul Solt on 6/10/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MapsAppPrototype/MapsAppPrototype/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapsAppPrototype
+//
+//  Created by Paul Solt on 6/10/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
